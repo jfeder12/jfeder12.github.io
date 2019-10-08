cddf13e18b64806be32ca7253ac33170d828a1d8
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2019-10-07 21:58:52 -0400

    complete form validation with jQuery

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function() {
+
+    // Change class of errors and place them after element's parent
+    $.validator.setDefaults({
+        errorClass: 'text-danger',
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+
+    // Custom email validation RegExp
+    $.validator.methods.email = function(value, element){
+        return this.optional( element ) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test( value );
+    }
+
+    // Validate form using jquery-validation library
+    $("form[name='emailForm']").validate({
+        rules: {
+            name: {
+                required: true,
+                lettersonly: true
+            },
+            _replyto:{
+                required: true,
+                email: true
+            },
+            message: "required"
+        },
+        messages: {
+            name: {
+                required: "Please enter your name",
+                lettersonly: "Please enter only letters"
+            },
+            _replyto: {
+                required: "Please enter your email",
+                email: "Please enter a valid email address"
+            },
+            message: "Please enter your message"
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
